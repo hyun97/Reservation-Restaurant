b5cb1648581f37ed0f4fec6ea771506da6515ce5
hyun97 <nkh1602@naver.com>
2020-01-10 19:35:30 +0900

    #5 가게 상세-2

--- project-api/src/main/java/com/project/reservation/interfaces/RestaurantController.java
+++ project-api/src/main/java/com/project/reservation/interfaces/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.project.reservation.interfaces;
 
 import com.project.reservation.domain.Restaurant;
+import com.project.reservation.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,8 @@ import java.util.List;
 @RestController
 public class RestaurantController {
 
+    RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
         List<Restaurant> restaurants = new ArrayList<>();
@@ -18,21 +21,11 @@ public class RestaurantController {
         restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
         restaurants.add(new Restaurant(2020L, "Cyber food", "Seoul"));
 
-        return restaurants;
+        return repository.findAll();
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-
-        return restaurant;
+        return repository.findById(id);
     }
 }
