f827c22a9bc5934829689e2378249c4b21a04f9a
hyun97 <nkh1602@naver.com>
2020-01-10 19:21:48 +0900

    #4 가게 상세-1

--- project-api/src/main/java/com/project/reservation/interfaces/RestaurantController.java
+++ project-api/src/main/java/com/project/reservation/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package com.project.reservation.interfaces;
 
 import com.project.reservation.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,10 +15,24 @@ public class RestaurantController {
     public List<Restaurant> list() {
         List<Restaurant> restaurants = new ArrayList<>();
 
-        Restaurant restaurant = null;
-
         restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber food", "Seoul"));
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber food", "Seoul"));
+
+        Restaurant restaurant = restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+        return restaurant;
     }
 }
