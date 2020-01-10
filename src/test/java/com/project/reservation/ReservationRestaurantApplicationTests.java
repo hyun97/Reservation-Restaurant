d9acb4b5e79439d9f1138aa4d45ec79b50bf9cc9
hyun97 <nkh1602@naver.com>
2020-01-10 15:05:28 +0900

    #0 READY

---
+++ src/test/java/com/project/reservation/ReservationRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
