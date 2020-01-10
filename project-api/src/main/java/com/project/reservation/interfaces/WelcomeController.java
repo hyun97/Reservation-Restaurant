2adbd68bf665766e4ef9419a3cce02d79d74992d
hyun97 <nkh1602@naver.com>
2020-01-10 15:12:59 +0900

    #1 Hello, World!

---
+++ project-api/src/main/java/com/project/reservation/interfaces/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.project.reservation.interfaces;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
+
+}
