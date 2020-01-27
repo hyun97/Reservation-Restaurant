1cd06a39b81a769cde26f6fd209fce3e5ce414b6
hyun97 <nkh1602@naver.com>
2020-01-27 11:24:14 +0900

    #41 회원가입-1

---
+++ customer-api/src/main/java/com/project/reservation/SecurityJavaConfig.java
@@ -0,0 +1,21 @@
+package com.project.reservation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityJavaConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable(); // iframe 사용가능
+    }
+
+}
